f9c2ac580a979c7feba1ab997943ea4d3058a86e
Harry Rollerson <hrollerson1@gmail.com>
2025-12-31 11:36:09 +0700

    Fix: restore rounded pill shape on both sides of ViewSelector toggle

--- src/app/(main)/discover/page.tsx
+++ src/app/(main)/discover/page.tsx
@@ -2352,7 +2352,7 @@ function DiscoverPageContent() {
                   >
                     <Filter className="h-4 w-4" />
                   </Button>
-                  <ViewSelector view={artworkView} onViewChange={setArtworkView} className="rounded-l-none border-l-0" />
+                  <ViewSelector view={artworkView} onViewChange={setArtworkView} className="rounded-l-md rounded-r-md border-l-2" />
                 </>
               ) : (
                 <>
